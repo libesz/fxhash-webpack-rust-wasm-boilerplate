82ec521921492147081f83a9747bb86d3a3a0c14
Gergő Huszty <Gergo.Huszty@ibm.com>
2023-05-10 16:49:10 +0200

    added rust webasm hello world alarm

--- project/src/index.js
+++ project/src/index.js
@@ -114,3 +114,12 @@ ${$fx.stringifyParams($fx.getRawParams())}
 ${$fx.stringifyParams($fx.getRawParams())}
 </pre>
 `
+
+// Note that a dynamic `import` statement here is required due to
+// webpack/webpack#6615, but in theory `import { greet } from './pkg';`
+// will work here one day as well!
+const rust = import('../../pkg');
+
+rust
+  .then(m => m.greet('World!'))
+  .catch(console.error);
